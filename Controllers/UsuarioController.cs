fe183f4a8471f443eab232b515c9c965afd05307
Cleiton <cleiton.siqueira.266@gmail.com>
2023-02-02 10:21:21 -0300

    Cadastro de usuarios

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -24,10 +24,39 @@ namespace ControleDeContatos.Controllers
             return View();
         }
 
+        public IActionResult Editar(int id)
+        {
+            UsuarioModel usuario  = _usuarioRepositorio.ListarPorId(id);
+            return View(usuario);
+        }
+
         public IActionResult ApagarConfirmacao(int id)
         {
             UsuarioModel usuario = _usuarioRepositorio.ListarPorId(id);
             return View(usuario);
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            try
+            {
+                bool apagado = _usuarioRepositorio.Apagar(id);
+
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Usuário apagado com sucesso";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Não foi possível apagar o usuário";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Não foi possível apagar o usuário, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
 
         [HttpPost]
